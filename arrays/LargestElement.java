d99ef6b752e45a999fb9703d906693980f1c9522
Rohit <rohitparih@gmail.com>
2025-11-10 15:55:24 +0530

    recursion -> `largest element`

---
+++ arrays/LargestElement.java
@@ -0,0 +1,48 @@
+package arrays;
+
+/*
+  Developer: Rohit Parihar
+  Project: DSA Core
+  GitHub: github.com/rohit-zip
+  File: LargestElement
+ */
+
+import java.util.Arrays;
+
+/**
+ * URL: https://takeuforward.org/data-structure/find-the-largest-element-in-an-array/
+ * Problem Statement: Given an array, we have to find the largest element in the array.
+ *
+ * Example 1:
+ * Input: arr[] = {2,5,1,3,0};
+ * Output: 5
+ * Explanation: 5 is the largest element in the array.
+ *
+ * Example2:
+ * Input: arr[] = {8,10,5,7,9};
+ * Output: 10
+ * Explanation: 10 is the largest element in the array.
+ */
+public class LargestElement {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 1, 3, 0};
+        System.out.println("The largest element in the array is: " + largestElement(arr));
+    }
+
+    private static int largestElement(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        for (int i : arr) {
+            //if (i > largest) largest = i;
+            largest = Math.max(largest, i);
+        }
+        return largest;
+    }
+
+    private static int usingStream(int[] arr) {
+        return Arrays
+                .stream(arr)
+                .max()
+                .orElse(Integer.MIN_VALUE);
+    }
+}
