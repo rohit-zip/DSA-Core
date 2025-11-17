92d966061ad7744f4db09372a6c224c5ccf3686a
Rohit <rohitparih@gmail.com>
2025-11-17 16:33:15 +0530

    recursion -> `rotate array`

--- arrays/RotateArray.java
+++ arrays/RotateArray.java
@@ -7,6 +7,18 @@ package arrays;
   File: RotateArray
  */
 
+/*
+ * Problem Statement:
+ * Given an integer array `arr` and an integer `k`, rotate the array to the right by `k` steps in-place.
+ * After rotation, each element in the array should be shifted to the right by `k` positions, and elements that fall off the end should wrap around to the beginning.
+ *
+ * Examples:
+ * Input: arr = [1, 2, 3, 4, 5], k = 2
+ * Output: [4, 5, 1, 2, 3]
+ *
+ * Input: arr = [7, 8, 9], k = 1
+ * Output: [9, 7, 8]
+ */
 import java.util.Arrays;
 
 public class RotateArray {
