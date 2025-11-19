f77f0a75106bce578e03bb941153764845a5f5ed
Rohit <rohitparih@gmail.com>
2025-11-19 17:08:03 +0530

    recursion -> `tower of hanoi`

--- recursion/TowerOfHanoi.java
+++ recursion/TowerOfHanoi.java
@@ -67,7 +67,7 @@ public class TowerOfHanoi {
      */
     private static void toh(int n, int t1, int t2, int t3) {
         if (n==0) return;
-        toh(n-1, t1, t3, t2);
+        toh(n-1 , t1, t3, t2);
         System.out.println(n + "[" + t1 + "->" + t2 + "]");
         toh(n-1, t3, t2, t1);
     }
