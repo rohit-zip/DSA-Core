fb6b9f3f977dac28ac2e1e3f5a73738f8ee870aa
Rohit <rohitparih@gmail.com>
2025-11-19 17:27:14 +0530

    recursion -> `display reverse`

---
+++ recursion/DisplayArrayReverse.java
@@ -0,0 +1,21 @@
+package recursion;
+
+/*
+  Developer: Rohit Parihar
+  Project: DSA Core
+  GitHub: github.com/rohit-zip
+  File: DisplayArrayReverse
+ */
+
+public class DisplayArrayReverse {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        displayReverse(arr, 0);
+    }
+
+    public static void displayReverse(int[] arr, int n) {
+        if (n == arr.length) return;
+        displayReverse(arr, n + 1);
+        System.out.print(arr[n] + " ");
+    }
+}
