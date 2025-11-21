6393c667df8b837a3aa554fcadd61359c4ab6a01
Rohit <rohitparih@gmail.com>
2025-11-21 19:19:51 +0530

    binary search -> `implement lower bound`

---
+++ binarySearch/ImplementLowerBound.java
@@ -0,0 +1,52 @@
+package binarySearch;
+
+/*
+  Developer: Rohit Parihar
+  Project: DSA Core
+  GitHub: github.com/rohit-zip
+  File: ImplementLowerBound
+ */
+
+/**
+ * Problem Statement: Given a sorted array of N integers and an integer x, write a program to find the lower bound of x.
+ * What is lower bound?
+ * The lower bound algorithm finds the first or the smallest index in a sorted array where the value at that index is greater than or equal to a given key i.e. x.
+ * The lower bound is the smallest index, ind, where arr[ind] >= x. But if any such index is not found, the lower bound algorithm returns n i.e. size of the given array.
+ *
+ * Example 1:
+ * Input Format: N = 4, arr[] = {1,2,2,3}, x = 2
+ * Result: 1
+ * Explanation: Index 1 is the smallest index such that arr[1] >= x.
+ *
+ * Example 2:
+ * Input Format: N = 5, arr[] = {3,5,8,15,19}, x = 9
+ * Result: 3
+ * Explanation: Index 3 is the smallest index such that arr[3] >= x.
+ */
+public class ImplementLowerBound {
+
+    public static void main(String[] args) {
+        int[] arr = {3,5,8,15,19};
+        int idx = solve(arr, 9);
+        System.err.println(idx);
+    }
+
+    private static int solve(int[] arr, int data) {
+        int f = -1;
+        int l = 0;
+        int h = arr.length - 1;
+        while (l <= h) {
+            int mid = (l + h) / 2;
+            if (data < arr[mid]) {
+                h = mid - 1;
+            } else if (data > arr[mid]) {
+                l = mid + 1;
+                f = mid;
+            } else {
+                f = mid;
+                break;
+            }
+        }
+        return f;
+    }
+}
