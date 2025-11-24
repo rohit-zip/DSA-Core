a7fd0118d303993d72b69c36d8907b2f9d249308
Rohit <rohitparih@gmail.com>
2025-11-24 14:14:13 +0530

    bubble sort

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -31,21 +31,20 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
-        optimized(arr);
+        bubbleSort(arr);
         for (int i : arr) {
             System.out.print(i + " ");
         }
     }
 
     private static void bubbleSort(int[] arr) {
-        int n = arr.length;
 
-        for (int i=n-1 ; i>0 ; i--) {
-            for (int j=0 ; j<i ; j++) {
-                if (arr[j] > arr[j+1]) {
+        for (int i=0 ; i<arr.length - 1 ; i++) {
+            for (int j = 1 ; j < arr.length - i ; j++) {
+                if (arr[j] < arr[j-1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
                 }
             }
         }
