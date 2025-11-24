45ad81317a94093eebd24c059baf14050e0369d6
Rohit <rohitparih@gmail.com>
2025-11-24 15:16:44 +0530

    merge two sorted arrays

---
+++ arrays/MergeTwoSortedArrays.java
@@ -0,0 +1,51 @@
+package arrays;
+
+/*
+  Developer: Rohit Parihar
+  Project: DSA Core
+  GitHub: github.com/rohit-zip
+  File: MergeTwoSortedArrays
+ */
+
+import java.util.Arrays;
+
+public class MergeTwoSortedArrays {
+
+    public static void main(String[] args) {
+        int[] arr1 = {2,5,12,18,20};
+        int[] arr2 = {2,7,9,11,15,25,28,30,35};
+        int[] solve = merge(arr1, arr2);
+        System.out.println(Arrays.toString(solve));
+    }
+
+    private static int[] merge(int[] arr1, int[] arr2) {
+        int[] arr = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                arr[k] = arr1[i];
+                i++;
+                k++;
+            } else {
+                arr[k] = arr2[j];
+                j++;
+                k++;
+            }
+        }
+
+        while (i < arr1.length) {
+            arr[k] = arr1[i];
+            i++;
+            k++;
+        }
+
+        while (j < arr2.length) {
+            arr[k] = arr2[j];
+            j++;
+            k++;
+        }
+
+        return arr;
+    }
+}
