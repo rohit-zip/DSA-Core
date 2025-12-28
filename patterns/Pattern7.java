aeda24d8a24b2c44cc0860e87fbd48b95e11b49e
Rohit <rohitparih@gmail.com>
2025-12-28 15:18:23 +0530

    Pattern 7

---
+++ patterns/Pattern7.java
@@ -0,0 +1,20 @@
+package patterns;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+        pattern7(5);
+    }
+
+    private static void pattern7(int n) {
+        int space = 0;
+        for (int i = 1 ; i <= n ; i++) {
+            for (int j = 1 ; j <= space ; j++) {
+                System.out.print("\t");
+            }
+
+            System.out.print("*\t");
+            space++;
+            System.out.println();
+        }
+    }
+}
